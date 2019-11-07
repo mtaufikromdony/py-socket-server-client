654bcb5016514de148df1b8566a5615e2ab5439f
Mochamad Taufik Romdony <mtaufik.romdony@gmail.com>
2019-11-07 19:34:53 +0700

    Update socket-server program

--- socket-server-py/socket-server.py
+++ socket-server-py/socket-server.py
@@ -31,3 +31,25 @@ def service_connection(key, mask):
             print("echoing", repr(data.outb), "to", data.addr)
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
+
+host = '0.0.0.0'
+port = 54321
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print("listening on", (host, port))
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
